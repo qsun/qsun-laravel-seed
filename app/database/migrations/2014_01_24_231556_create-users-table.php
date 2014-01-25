dbab4d7cb9ac2bfd57e0c02ca8348a6003011521
Quan Sun <cfk.quan@gmail.com>
2014-01-25 13:38:48 +1100

    user table migration script

--- app/database/migrations/2014_01_24_231556_create-users-table.php
+++ app/database/migrations/2014_01_24_231556_create-users-table.php
@@ -4,24 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		//
-	}
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function ($t) {
+      $t->increments('id');
+      $t->string('email', 100)->unique();
+      $t->string('password', 64);
+      $t->timestamps();
+    });
+	//
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('users');
+  }
 
 }
